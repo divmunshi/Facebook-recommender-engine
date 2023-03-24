0bbc9dd004771336c728a190ed3bdd0ef042568f
root <root@ip-172-31-82-5.ec2.internal>
2023-03-24 15:47:27 +0000

    Merging to main

---
+++ migrations/pg.sql
@@ -0,0 +1,52 @@
+CREATE TABLE users (
+    user_id VARCHAR(255) PRIMARY KEY,
+    age INT,
+    lang VARCHAR(50),
+    location VARCHAR(255),
+    created_at TIMESTAMP,
+    tag VARCHAR(255)
+);
+
+CREATE TABLE sessions (
+    session_id VARCHAR(255) PRIMARY KEY,
+    user_id VARCHAR(255),
+    session_end TIMESTAMP,
+    session_start TIMESTAMP,
+    rec_count INT,
+    duration INTERVAL
+);
+
+CREATE TABLE items (
+    item_id VARCHAR(255) PRIMARY KEY,
+    item_key VARCHAR(255),
+    user_id VARCHAR(255),
+    bucket_key VARCHAR(255),
+    content_type VARCHAR(255),
+    created_at TIMESTAMP,
+    tag VARCHAR(255)
+);
+
+CREATE TABLE recommendations (
+    item_id VARCHAR(255),
+    session_id VARCHAR(255),
+    user_id VARCHAR(255),
+    engagement_duration INTERVAL,
+    is_final BOOLEAN,
+    order_in_session INT,
+    sent_at TIMESTAMP
+);
+
+CREATE INDEX idx_users_user_id ON users(user_id);
+CREATE INDEX idx_users_created_at ON users(created_at);
+
+CREATE INDEX idx_sessions_user_id ON sessions(user_id);
+
+CREATE INDEX idx_items_item_id ON items(item_id);
+CREATE INDEX idx_items_item_key ON items(item_key);
+CREATE INDEX idx_items_user_id ON items(user_id);
+CREATE INDEX idx_items_created_at ON items(created_at);
+
+CREATE INDEX idx_recommendations_item_id ON recommendations(item_id);
+CREATE INDEX idx_recommendations_session_id ON recommendations(session_id);
+CREATE INDEX idx_recommendations_user_id ON recommendations(user_id);
+CREATE INDEX idx_recommendations_sent_at ON recommendations(sent_at);
